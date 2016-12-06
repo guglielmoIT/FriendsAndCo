86f2be8f573fad38b22b97be395f497cabd19607
guglielmoIT <guglielmo.puglia@innovationtech.it>
2016-12-06 12:29:47 +0100

    finito la maschera delle note . manca la logica

---
+++ friendsAndCo/MyShop/Model/opzioniGruppi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace FriendsAndCo
+{
+	public class opzioniGruppi
+	{
+		int id;
+		string descrizione;
+		int deleted;
+		int automatico;
+
+		[JsonProperty(PropertyName = "id")]
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		[JsonProperty(PropertyName = "descrizione")]
+		public string Descrizione
+		{
+			get { return descrizione; }
+			set { descrizione = value; }
+		}
+
+		[JsonProperty(PropertyName = "deleted")]
+		public int Deleted
+		{
+			get { return deleted; }
+			set { deleted = value; }
+		}
+
+		[JsonProperty(PropertyName = "automatico")]
+		public int Automatico
+		{
+			get { return automatico; }
+			set { automatico = value; }
+		}
+
+		[Version]
+		public string Version { get; set; }
+	}
+}
+
---
+++ friendsAndCo/MyShop/Views/Liste/Allergie.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FriendsAndCo
+{
+	public partial class Allergie : ContentPage
+	{
+		ListaItem paramLista = new ListaItem();
+
+		OpzioniGruppiManager opzManager = OpzioniGruppiManager.DefaultManager;
+
+		public Allergie(ListaItem lst)
+		{
+			InitializeComponent();
+			paramLista = lst;
+
+			titolo.Text = paramLista.Operazione;
+
+			//nel campo deleted è dichiarato il parametro automatico
+
+
+
+			if (titolo.Text == "Personalizzato")
+			{
+				addLista.IsVisible = true;
+			}
+			else
+			{
+				addLista.IsVisible = false;
+			}
+
+
+
+
+		}
+
+		public Allergie()
+		{
+			InitializeComponent();
+
+		}
+
+		void OnAdd(object sender, System.EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void OnSelected(object sender, System.EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void OnRefresh(object sender, System.EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
